d75b36c6fd0b3db4a14d25f28e77795d06a8fd5a
John Wallace <jnwywe@gmail.com>
2018-03-23 16:34:20 -0500

    finished array exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,4 +1,29 @@
 "use strict";
 
-prompt("enter a number between 1 and 50");
+function isOdd(x) {
+    return x % 2 === 1;
+}
+var number;
 
+do{
+  number =  parseFloat(prompt("enter an odd number between 1 and 50"));
+  if(isOdd(number)){
+      break;
+  }
+}while(true);
+
+console.log(number);
+
+//=================================================================================
+
+
+for( var i = 1; i <= 50; i++) {
+
+    if(i === number) {
+        console.log("yikes skipping number " + i)
+        continue;
+    }
+    if (isOdd(i)) {
+        console.log( "here is an odd number " + i);
+    }
+}
--- js/for_loops.js
+++ js/for_loops.js
@@ -13,8 +13,11 @@ console.log(showMultiplicationTable(3));
  for (var i = 100; i >= 5; i = i - 5) {
      console.log(i);
  }
- //---------------------------------------------------
-
+ //===================================================================================
+for(var i = 1; i <= 9; i++){
+     i = i.toString();
+     console.log(i.repeat(i));
+}
 
 
 //-----------------------------------------------------------
@@ -33,10 +36,13 @@ function isOdd(x) {
 
 var number;
  for (var i = 1; i <= 10; i++) {
-     number = randomint(20, 200);
+     number = randomInt(20, 200);
 
      if(isEven(number)) {
          console.log(number + " is even");
+
+     } if(isOdd(number)) {
+         console.log(number + " is odd");
 
      }
  }
