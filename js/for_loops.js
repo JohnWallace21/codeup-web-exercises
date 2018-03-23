2255399a0f77bbb41fb4e7d2a0fb3ec75006089f
John Wallace <jnwywe@gmail.com>
2018-03-23 14:03:41 -0500

    updated for loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,43 @@
 "use strict";
 
-
-function showMultiplicationTable(x){
-
+function showMultiplicationTable(number) {
+    for (var i = 1; i <= 10; i++)
+        console.log( number +"x" + i + "=" + (number * i));
 }
+
+
+console.log(showMultiplicationTable(3));
+
+
+//---------------------------------------------------------------------
+ for (var i = 100; i >= 5; i = i - 5) {
+     console.log(i);
+ }
+ //---------------------------------------------------
+
+
+
+//-----------------------------------------------------------
+
+function randomInt( start, end){
+     return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function isEven(x) {
+     return x % 2 === 0;
+}
+function isOdd(x) {
+     return x % 2 === 1;
+}
+
+
+var number;
+ for (var i = 1; i <= 10; i++) {
+     number = randomint(20, 200);
+
+     if(isEven(number)) {
+         console.log(number + " is even");
+
+     }
+ }
+
--- js/while.js
+++ js/while.js
@@ -1,4 +1,6 @@
 "use strict";
+//1. we are always multiplying the initial number by two
+
 
 
 var i = 2;
@@ -9,7 +11,12 @@ while (i < 65537) {
 }
 
 
+
 //---------------------------------------------------------------------------------------------
+
+
+
+
 
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
@@ -27,6 +34,8 @@ var conesPurchased = Math.floor(Math.random() * 5) + 1;
      }
 
 } while(allCones > 0);
+
+console.log("yay all the cones are gone");
 
 
 
