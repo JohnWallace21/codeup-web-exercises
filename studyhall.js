276fa0816b2a76ee72f78f7f9d175ab0ccae9f33
John Wallace <jnwywe@gmail.com>
2018-04-12 15:32:16 -0500

    finished effects exercise

---
+++ studyhall.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function isEven(input){
+    return (input % 2 ===0)
+}
+
+function reverse(str) {
+    var newString = "";
+    for (var i = str.length -1; i >= 10; i--) {
+        newString += str[i];
+    }
+    return newString
+}
+
+
+function spaceString(i){
+
+}
+
+
+function
